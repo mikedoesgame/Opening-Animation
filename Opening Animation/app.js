550b108e9c62229f5aa50321c53fd4cc92e886df
Mike Lavallee <51940050+mikedoesgame@users.noreply.github.com>
2021-01-12 16:13:42 -0500

    First Branch
    Pushing to Github

---
+++ Opening Animation/app.js
@@ -0,0 +1,13 @@
+//calling GSAP
+const tl = gsap.timeline({ defaults: { ease: "power1.out"} });
+
+//Animating Intro Text and Slider
+tl.to(".text", { y:'0%', duration: 1, stagger: 0.25 });
+tl.to(".slider", { y: "-100%", duration: 1.5, delay: 0.5 });
+tl.to(".intro", { y: "-100%", duration: 1,}, "-=1" );
+
+//Animates Navbar
+tl.fromTo("nav", {opacity: 0}, {opacity: 1, duration: 1});
+
+//Animates Text
+tl.fromTo(".big-text", {opacity: 0}, {opacity: 1, duration: 1}, "-=1" );
